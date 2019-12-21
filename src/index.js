52ea774b9ba7c942508ac2edced7a2d549faf615
iKurisu <chris.epic01@gmail.com>
2019-12-21 18:09:18 +0100

    add cmd interface

--- src/index.js
+++ src/index.js
@@ -1,5 +1,50 @@
-const { fetchSubreddit } = require("./api/index");
+const inquirer = require("inquirer");
+const chalk = require("chalk");
+const { fetchSubreddit, fetchPost, fetchComments } = require("./api/index");
 
-fetchSubreddit(
-  "learnprogramming"
-).then(console.log);
+inquirer
+  .prompt([
+    {
+      name: "subreddit",
+      message: "Which subreddit would you like to fetch?",
+      validate: async input => {
+        const response = await fetchSubreddit(input);
+        if (typeof response === "object") {
+          return true;
+        }
+
+        return response;
+      }
+    },
+    {
+      type: "list",
+      name: "permalink",
+      message: "Which post would you like to see?",
+      choices: async answer => {
+        const posts = await fetchSubreddit(answer.subreddit);
+
+        return posts.map(post => ({ name: post.title, value: post.permalink }));
+      },
+      pageSize: 50
+    }
+  ])
+  .then(async answers => {
+    const post = await fetchPost(answers.permalink);
+    
+    console.log(post);
+
+    inquirer
+      .prompt([
+        { type: "confirm", name: "loadComments", message: "Load comments?" }
+      ])
+      .then(async () => {
+        const comments = await fetchComments(answers.permalink);
+
+        comments.forEach((comment, i) =>
+          console.log(
+            `${comment.data.body}\n` +
+              chalk.yellow(`by ${comment.data.author}\n`)
+          )
+        );
+      });
+  });
